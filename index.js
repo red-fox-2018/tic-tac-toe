31ecda7f90e06aac5695a27bc6a5a68eed5db571
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-04-04 12:59:45 +0700

    perbaikan result

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ function jelangkung(col,row){
     for (var j = 0; j < col; j++) {
       let random = Math.round(Math.random()*1)
       if(random==1 && X<=4){
-        result[i].push(['X'])
+        result[i].push('X')
         X++
         //console.log(j,'X',X);
       }else if(random==0 && O <=4 ){
-          result[i].push(['O'])
+          result[i].push('O')
           O++
           //console.log(j,'O',O);
       }else if (random==1 && X == 5){
-        result[i].push(['O'])
+        result[i].push('O')
       }else if (random==0 && O == 5){
-        result[i].push(['X'])
+        result[i].push('X')
       }
     }
   }
