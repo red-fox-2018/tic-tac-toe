c9d1c7cf2e7d60b94043c526ee4dcd74f6ddac79
Iswanul Umam <umam@sepulsa.com>
2018-04-04 11:55:08 +0700

    finish

---
+++ index.js
@@ -0,0 +1,38 @@
+
+/*
+
+@ Iswanul Umam - Red Fox
+*/
+
+function generateRandomXO() {
+  let str = ``;
+  let counter = { 'X': 0, 'O': 0 };
+
+  while (str.length < 9) {
+    let indexRandom = Math.round(Math.random()); // 0 or 1
+    let item = ['X', 'O'];
+    if (counter[item[indexRandom]] < 5) {
+      str += item[indexRandom];
+      counter[item[indexRandom]]++;
+    }
+  }
+  return str.split('');
+}
+
+function boardTicTacToe() {
+  let board = [];
+  let newBoard = generateRandomXO();
+
+  for (let i = 0; i < 3; i++) {
+    board[i] = [];
+    for (let j = 0; j < 3; j++) {
+      console.log(newBoard);
+      board[i].push(newBoard[0]);
+      newBoard.shift();
+    }
+  }
+
+  return board;
+}
+
+console.log(boardTicTacToe());
